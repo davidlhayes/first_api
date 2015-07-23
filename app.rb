49f63378cd7997ecabbde56f1907e526fe5903fe
David Hayes <davidlynnhayes@att.net>
2015-07-23 14:33:04 -0500

    fixed bundler

--- app.rb
+++ app.rb
@@ -1,9 +1,13 @@
 require 'bundler'
 Bundler.require()
 
+def some_json
+  {:name => 'Darth Vader', :message => 'The force is strong with this one'}.to_json
+end
+
 # http://localhost/
 get '/' do
-  {:name => 'Darth Vader', :message => 'The force is strong with this one'}.to_json
+  erb :index #erb says, hey! go find 'index.erb'
 end
 
 # http://localhost/api/awesome/
@@ -14,4 +18,9 @@ get '/api/awesome' do
    :male_lead => 'Paul Reiser',
    :genre => 'Sci-Fi'}.to_json
 
+end
+
+# get: localhost/api/json
+get '/api/json' do
+  some_json
 end
