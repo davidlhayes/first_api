49d8d58b6b11d5072b03d33beb59c34e68614a08
David Hayes <davidlynnhayes@att.net>
2015-07-23 12:23:39 -0500

    first commit

---
+++ app.rb
@@ -0,0 +1,13 @@
+require 'Bundler'
+Bundler.require()
+
+# http://localhost/
+get '/' do
+  {:name => 'Darth Vader', :message => 'The force is strong with this one'}.to_json
+end
+
+# http://localhost/api/awesome/
+get '/api/awesome' do
+  #do something else
+
+end
